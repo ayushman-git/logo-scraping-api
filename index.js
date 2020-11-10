25c74c13321736d6b965815bc49bc5371f8f0110
Ayushman Gupta <ayushman1998.1998@gmail.com>
2020-11-10 12:13:15 +0530

    added req

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { LogoScrape } = require("logo-scrape");
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = new express();
 
 const port = process.env.PORT || 3000;
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("<h2>Server is running.</h2>");
